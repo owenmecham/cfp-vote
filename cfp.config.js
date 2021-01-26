7eaa8ca7d63072fa42681bb69f54851e0acd9895
owenmecham <owen.mecham@gmail.com>
2021-01-25 20:33:39 -0700

    feat: updated title

--- cfp.config.js
+++ cfp.config.js
@@ -1,22 +1,22 @@
 module.exports = {
-	title: "CFP Vote",
-	votingStages: {
-		'stage_1': {
-			label: 'First Voting Round',
-			voteUI: [
-				{ label: 'nope', 		value: 0 },
-				{ label: 'seen better',	value: 2 },
-				{ label: 'maybe', 		value: 4 },
-				{ label: 'absolutely', 	value: 8 },
-			],
-		},
-		'stage_2': {
-			label: 'Shortlisting Round',
-			voteUI: [
-				{ label: 'meh', value: 1 },
-				{ label: 'yay', value: 2 },
-				{ label: 'MUST', value: 3 }
-			]
-		}
-	},
+  title: "ng-conf CFP",
+  votingStages: {
+    'stage_1': {
+      label: 'First Voting Round',
+      voteUI: [
+        { label: 'nope', value: 0 },
+        { label: 'seen better', value: 2 },
+        { label: 'maybe', value: 4 },
+        { label: 'absolutely', value: 8 },
+      ],
+    },
+    'stage_2': {
+      label: 'Final Round',
+      voteUI: [
+        { label: 'meh', value: 1 },
+        { label: 'yay', value: 2 },
+        { label: 'MUST', value: 3 }
+      ]
+    }
+  },
 }
